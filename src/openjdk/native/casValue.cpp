7507cdab5f42ee9929b5b3e7122c0b9e310db8db
TJX2014 <xiaoxingstack@gmail.com>
2021-05-16 23:12:48 +0800

    cas

--- src/openjdk/native/casValue.cpp
+++ src/openjdk/native/casValue.cpp
@@ -6,10 +6,14 @@ template<typename T>
 T changeValue11(T exchange_value,
                                                 T volatile* dest,
                                                 T compare_value) {
-  __asm__ volatile ("lock cmpxchgl %1,(%3)"
+  // __asm__ volatile ("lock cmpxchgl %1,(%3)"
+  //                   : "=a" (exchange_value)
+  //                   : "r" (exchange_value), "a" (compare_value), "r" (dest)
+  //                   : "cc", "memory");
+   __asm__ volatile ("lock cmpxchgl %1,(%3)"
                     : "=a" (exchange_value)
                     : "r" (exchange_value), "a" (compare_value), "r" (dest)
-                    : "cc", "memory");
+   );
   return exchange_value;
 }
 
@@ -17,7 +21,7 @@ int main() {
   int exchange_value=10;
   // exchange_value will take effect only when dest is equal to compare_value
   int dest = 21;
-  int compare_value = 20;
+  int compare_value = 21;
   changeValue11(exchange_value, &dest, compare_value);
   printf("%d", dest);
 }
